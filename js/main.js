07c28f41ff773628edc3d0deaeb642a1a4e84283
tf-ferreira <77362611+tf-ferreira@users.noreply.github.com>
2021-01-12 19:13:25 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
+
+function init(el){
+    document.getElementById(el).style.display = 'none';
+}
+function Mudarestado(el) {
+        var display = document.getElementById(el).style.display;
+        if(display == "none")
+            document.getElementById(el).style.display = 'block';
+        else
+            document.getElementById(el).style.display = 'none';
+}
